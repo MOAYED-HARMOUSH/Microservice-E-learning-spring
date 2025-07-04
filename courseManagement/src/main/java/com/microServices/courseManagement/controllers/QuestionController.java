e4be941ec2e96b67e4b98ba44caca1d8dd3b1802
MOAYED-HARMOUSH <moayad.harmoush@gmail.com>
2025-07-05 01:51:38 +0300

    stable questions crud

--- courseManagement/src/main/java/com/microServices/courseManagement/controllers/QuestionController.java
+++ courseManagement/src/main/java/com/microServices/courseManagement/controllers/QuestionController.java
@@ -11,7 +11,7 @@ import javax.validation.Valid;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/questions")
+@RequestMapping("/api/courses/questions")
 public class QuestionController {
     
     @Autowired
